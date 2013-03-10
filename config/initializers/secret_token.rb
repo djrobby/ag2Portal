972c33d3a91a9df26bdf5aa854ef5be230dbdfe3
spsisys <spsisys@gmail.com>
2013-03-10 21:21:45 +0100

    Project Creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ag2Portal::Application.config.secret_token = 'c857ae58ddb575fc6c6c00c7899573bd1180748a7b127657165cd8dd32f6418f61039a1d04899880de8c1ea30af8ece1facee10c2f9392907a1f252c6db99cbf'
