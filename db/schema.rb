6a0e700f715a4a545d610330775c47557f8330bd
spsisys <spsisys@gmail.com>
2013-03-17 11:51:52 +0100

    Polished show user display

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(:version => 20130307181026) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "name"
+    t.string   "name",                   :default => "", :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
