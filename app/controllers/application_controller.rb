4f560ad58a3bef712e315ae413c61829db355a41
spsisys <spsisys@gmail.com>
2013-03-23 13:42:05 +0100

    Style to new user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,22 @@ class ApplicationController < ActionController::Base
   private
 
   def layout
-    # turn it off for login pages:
-    is_a?(Devise::SessionsController) ? "login" : "application"
+    # turn layout off for session and registration pages:
+    if is_a?(Devise::SessionsController)
+      "session"
+    elsif is_a?(Devise::RegistrationsController)
+      "registration"
+    else
+      "application"
+    end
+    
+    # turn it off for registration pages:
+    # is_a?(Devise::SessionsController) ? "session" : "application"
+    # turn it off for registration pages:
+    # is_a?(Devise::RegistrationsController) ? "registration" : "application"
     # or turn layout off for every devise controller:
     # devise_controller? && "application"
-    # turn it off for welcome page:
-    # in the controller
+    
+    # turn it off for welcome page: Controller
   end
 end
