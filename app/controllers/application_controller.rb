63a6614bbfc2e6436a57e73b5861d77eb36e28be
spsisys <spsisys@gmail.com>
2013-04-30 13:49:35 +0200

    Swith between languages added at footnav - Locale detected at application controller from http request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,20 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    # I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
   end
 
   def default_url_options(options={})
-    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    # logger.debug "default_url_options is passed options: #{options.inspect}\n"
     { :locale => I18n.locale }
   end
 
   private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 
   def layout
     # turn layout off for session and registration pages:
