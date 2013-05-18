fa8f034d849f04b6df50670e79e8eaab3608e61d
spsisys <spsisys@gmail.com>
2013-05-18 08:10:11 +0200

    Preparing deployment 1

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
+  gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
@@ -38,6 +38,7 @@ gem 'jquery-rails'
 
 # Deploy with Capistrano
 gem 'capistrano'
+gem 'rvm-capistrano'
 
 # To use debugger
 # gem 'debugger'
@@ -55,6 +56,7 @@ gem 'dbf'
 gem 'will_paginate', '~> 3.0.0'
 gem 'omniauth-google-apps'
 gem 'omniauth-google-oauth2'
+gem 'libv8'
 
 # Own required gems
 gem 'ag2_admin', :path => 'engines/ag2Admin'
---
+++ config/deploy.rb
@@ -0,0 +1,44 @@
+# Choose a Ruby explicitly, or read from an environment variable.
+set :rvm_ruby_string, :local               # use the same ruby as used locally for deployment
+set :rvm_type, :user
+set :rvm_autolibs_flag, "read-only"        # more info: rvm help autolibs
+
+before 'deploy', 'rvm:install_rvm'  # update RVM
+before 'deploy', 'rvm:install_ruby' # install Ruby and create gemset (both if missing)
+
+# Load RVM's capistrano plugin.
+require 'rvm/capistrano'
+
+# be sure to change these
+set :user, 'nestor'
+set :domain, 'ag2front'
+set :application, 'agestiona2'
+
+# file paths
+set :repository,  "#{user}@#{domain}:git/#{application}.git"
+set :deploy_to, "/home/#{user}/#{application}"
+
+# distribute your applications across servers (the instructions below put them
+# all on the same server, defined above as 'domain', adjust as necessary)
+role :app, domain
+role :web, domain
+role :db, "ag2back", :primary => true
+
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+# miscellaneous options
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :use_sudo, false
+set :bundle_flags,    ""
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
