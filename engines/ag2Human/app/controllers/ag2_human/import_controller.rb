f367f36f9e15286d688845ffdc8550ec54cc2c02
spsisys <spsisys@gmail.com>
2013-06-23 10:58:26 +0200

    Workers import goes on - Added ok_with_errors to locales

--- engines/ag2Human/app/controllers/ag2_human/import_controller.rb
+++ engines/ag2Human/app/controllers/ag2_human/import_controller.rb
@@ -4,9 +4,9 @@ require "dbf"
 module Ag2Human
   class ImportController < ApplicationController
     before_filter :authenticate_user!
+    before_filter :set_defaults
     # load_and_authorize_resource :worker
     skip_load_and_authorize_resource :only => :data_import
-
     def index
       authorize! :update, Worker
     end
@@ -22,6 +22,7 @@ module Ag2Human
       data_import_config = DataImportConfig.find_by_name('workers')
       source = source_exist(data_import_config.source, nil)
       if source.nil?
+        message = I18n.t("result_error_message_html", :scope => :"ag2_human.import.index")
         @json_data = { "DataImport" => message, "Result" => "ERROR" }
         render json: @json_data
         return
@@ -36,18 +37,22 @@ module Ag2Human
           if !source.nil?
             trabaja = DBF::Table.new(source + "trabaja.dbf")
             trabaja.each do |t|
+              if t.CEMAIL.blank?
+                next
+              end
               nomina_id = e.ccodemp + '-' + t.ccodtra
               worker = Worker.find_by_nomina_id(nomina_id)
               if worker.nil?
                 # Add new worker from source
-                #worker = Worker.new
-                #worker.nomina_id = nomina_id
+                # worker = Worker.new
+                # worker.nomina_id = nomina_id
               end
               #update_worker(worker, t)
               #if !worker.save
-              #  message = "Error: Workers Updater finished unexpectedly!".html_safe
-              #  @json_data = { "DataImport" => message, "Result" => "ERROR" }
-              #  break
+                # Error: Workers Updater finished unexpectedly!
+                # message = I18n.t("result_error_message_html", :scope => :"ag2_human.import.index")
+                # @json_data = { "DataImport" => message, "Result" => "ERROR" }
+                # break
               #end
             end
           end
@@ -57,74 +62,74 @@ module Ag2Human
     end
 
     def commented_code
-=begin      
-      # Copy 'empresa.dbf' file
-      data_import_config = DataImportConfig.find_by_name('workers')
-      source = source_exist(data_import_config.source, nil)
-      if source.nil?
-        @json_data = { "DataImport" => message, "Result" => "ERROR" }
-        render json: @json_data
-      end
-      target = Rails.root.to_s + data_import_config.target
-      create_target_dir(target)
-      FileUtils.cp(Dir[source + 'empresa.dbf'], target)
-      
-      # Loop thru 'empresa' records
-      empresa = DBF::Table.new(target + "empresa.dbf")
-      empresa.each do |record|
-        company = Company.find_by_fiscal_id(record.ccif)
-        if !company.nil?
-          # Copy 'trabaja.dbf' file for current 'empresa'
-          source = source_exist(data_import_config.source, record.ccodemp)
-          if !source.nil?
-            target = Rails.root.to_s + data_import_config.target + 'Emp' + record.ccodemp + '/'
-            create_target_dir(target)
-            FileUtils.cp(Dir[source + 'trabaja.dbf'], target)
-          end
-        end
-      end
-      
-      render json: @json_data
+=begin
+# Copy 'empresa.dbf' file
+data_import_config = DataImportConfig.find_by_name('workers')
+source = source_exist(data_import_config.source, nil)
+if source.nil?
+@json_data = { "DataImport" => message, "Result" => "ERROR" }
+render json: @json_data
+end
+target = Rails.root.to_s + data_import_config.target
+create_target_dir(target)
+FileUtils.cp(Dir[source + 'empresa.dbf'], target)
 
-      @sources = Worker.all
+# Loop thru 'empresa' records
+empresa = DBF::Table.new(target + "empresa.dbf")
+empresa.each do |record|
+company = Company.find_by_fiscal_id(record.ccif)
+if !company.nil?
+# Copy 'trabaja.dbf' file for current 'empresa'
+source = source_exist(data_import_config.source, record.ccodemp)
+if !source.nil?
+target = Rails.root.to_s + data_import_config.target + 'Emp' + record.ccodemp + '/'
+create_target_dir(target)
+FileUtils.cp(Dir[source + 'trabaja.dbf'], target)
+end
+end
+end
 
-      @sources.each do |source|
-        @worker = Worker.find_by_worker_id(source)
-        if @worker.nil?
-          # Add new worker from source
-          @worker = Worker.new
-          @worker.worker_id = source.id
-        end
-        update_worker(@worker, source)
-        if @worker.save
-          message = "Error: Workers Updater finished unexpectedly!".html_safe
-          @json_data = { "DataImport" => message, "Result" => "ERROR" }
-          render json: @json_data
-        end
-      end
-      sleep 1
-      render json: @json_data
+render json: @json_data
+
+@sources = Worker.all
+
+@sources.each do |source|
+@worker = Worker.find_by_worker_id(source)
+if @worker.nil?
+# Add new worker from source
+@worker = Worker.new
+@worker.worker_id = source.id
+end
+update_worker(@worker, source)
+if @worker.save
+message = "Error: Workers Updater finished unexpectedly!".html_safe
+@json_data = { "DataImport" => message, "Result" => "ERROR" }
+render json: @json_data
+end
+end
+sleep 1
+render json: @json_data
 =end
     end
 
     def dir_exist(dir)
       if !File.exist?(dir) || !File.directory?(dir)
-        return false
+      return false
       else
-        return true
+      return true
       end
     end
-    
+
     def source_exist(dir, emp)
       from = dir + 'Emp'
       if !emp.nil?
-        from = from + emp
+      from = from + emp
       end
       from += '/'
       if !dir_exist(from)
         from = dir + 'EMP'
         if !emp.nil?
-          from = from + emp
+        from = from + emp
         end
         from += '/'
         if !dir_exist(from)
@@ -133,25 +138,95 @@ module Ag2Human
       end
       return from
     end
-        
+
     def create_target_dir(dir)
       if !dir_exist(dir)
         FileUtils.mkdir(dir)
       end
     end
-    
+
+    def set_defaults
+      @street_type = StreetType.first
+      @company = Company.first
+      @office = Office.find_by_company_id(@company)
+      @department = Department.first
+      @professional_group = ProfessionalGroup.first
+      @contract_type = ContractType.first
+      @collective_agreement = CollectiveAgreement.first
+      @zipcode = Zipcode.first
+      @worker_type = WorkerType.first
+      @degree_type = DegreeType.first
+    end
+
     def update_worker(worker, source)
-      worker.first_name = source.first_name unless source.first_name.nil?
-      worker.last_name = source.last_name unless source.last_name.nil?
-      worker.company_id = source.company_id unless source.company_id.nil?
-      worker.office_id = source.office_id unless source.office_id.nil?
-      worker.department_id = source.department_id unless source.department_id.nil?
-      worker.position = source.position unless source.position.nil?
-      worker.email = source.email unless source.email.nil?
-      worker.corp_phone = source.corp_phone unless source.corp_phone.nil?
-      worker.corp_extension = source.corp_extension unless source.corp_extension.nil?
-      worker.corp_cellular_long = source.corp_cellular_long unless source.corp_cellular_long.nil?
-      worker.corp_cellular_short = source.corp_cellular_short unless source.corp_cellular_short.nil?
+      worker.first_name = source.CNOMTRA unless source.CNOMTRA.blank?
+      worker.last_name = source.CAPETRA unless source.CAPETRA.blank?
+      worker.fiscal_id = source.CDNI unless source.CDNI.blank?
+      worker.borned_on = source.DFECNAC unless source.DFECNAC.blank?
+      worker.street_type_id = @street_type.id unless @street_type.id.blank?
+      worker.street_name = source.CDIRCEN unless source.CDIRCEN.blank?
+      worker.street_number = source.CNUMCEN unless source.CNUMCEN.blank?
+      worker.floor = source.CPISO unless source.CPISO.blank?
+      worker.floor_office = source.CPUERTA unless source.CPUERTA.blank?
+      worker.zipcode_id = @zipcode.id unless @zipcode.id.blank?
+      worker.town_id = @zipcode.town_id unless @zipcode.town_id.blank?
+      worker.province_id = @zipcode.province_id unless @zipcode.province_id.blank?
+      worker.company_id = @company.id unless @company.id.nil?
+      worker.office_id = @office.id unless @office.id.nil?
+      worker.department_id = @department.id unless @department.id.nil?
+      worker.professional_group_id = @professional_group.id unless @professional_group.id.nil?
+      worker.position = source.CPUESTO unless source.CPUESTO.nil?
+      worker.starting_at = source.DFECALTA unless source.DFECALTA.blank?
+      worker.issue_starting_at = source.DFECINI unless source.DFECINI.blank?
+      worker.gross_salary = source.NBRUTO unless source.NBRUTO.blank?
+      worker.affiliation_id = source.CNUMSEG unless source.CNUMSEG.blank?
+      worker.contribution_account_code = source.CSUBCTA unless source.CSUBCTA.blank?
+      worker.own_phone = source.CTELEFONO unless source.CTELEFONO.blank?
+      worker.email = source.CEMAIL unless source.CEMAIL.blank?
+      worker.contract_type_id = @contract_type.id unless @contract_type.id.nil?
+      worker.collective_agreement_id = @collective_agreement.id unless @collective_agreement.id.nil?
+      worker.collective_agreement_id = @collective_agreement.id unless @collective_agreement.id.nil?
+      worker.worker_type_id = @worker_type.id unless @worker_type.id.nil?
+      worker.degree_type_id = @degree_type.id unless @degree_type.id.nil?
+      worker.worker_code = generate_worker_code(source.CAPETRA, source.CNOMTRA)
+      worker.user_id = @user.id unless @user.id.nil?
+    end
+
+    def generate_worker_code(lastname, firstname)
+      code = ''
+      lastname1 = lastname.split(" ").first
+      lastname2 = lastname.split(" ").last
+
+      # Builds code, if possible
+      if !lastname1.blank? && lastname1.length > 1
+        code += lastname1[0, 2]
+      end
+      if !lastname2.blank? && lastname2.length > 1
+        code += lastname2[0, 2]
+      end
+      if !firstname.blank? && firstname.length > 0
+        code += firstname[0, 1]
+      end
+
+      if code == ''
+        code = 'LLNNF'
+      else
+        if code.length < 5
+          code = code.ljust(5, '0')
+        end
+      end
+      code.upcase!
+      
+      # Code must be unique
+      s = 0
+      w = Worker.find_by_worker_code(code)
+      until w.nil?
+        code = code[0, 5] + s.to_s
+        s += 1
+        w = Worker.find_by_worker_code(code)
+      end
+      
+      return code
     end
   end
 end
