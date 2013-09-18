23924a2d4dff20b822db537a80df2d1d8404a8f0
spsisys <spsisys@gmail.com>
2013-09-18 10:44:33 +0200

    Remarks added to worker model

--- app/models/worker.rb
+++ app/models/worker.rb
@@ -23,7 +23,7 @@ class Worker < ActiveRecord::Base
                   :contribution_account_code, :position, :corp_phone,
                   :corp_cellular_long, :corp_cellular_short, :corp_extension,
                   :department_id, :nomina_id, :gross_salary, :variable_salary,
-                  :created_by, :updated_by
+                  :created_by, :updated_by, :remarks
   has_attached_file :avatar, :styles => { :medium => "96x96>", :small => "64x64>" }, :default_url => "/images/missing/:style/user.png"
 
   has_paper_trail
