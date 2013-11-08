c867921d411469f20c9ab1dd02a653e7ed1e5b4b
spsisys <spsisys@gmail.com>
2013-11-08 12:41:52 +0100

    Fixing DataImport for Workers (latin alfabet to UTF-8)

--- engines/ag2Human/app/controllers/ag2_human/import_controller.rb
+++ engines/ag2Human/app/controllers/ag2_human/import_controller.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 require_dependency "ag2_human/application_controller"
 require "dbf"
 
@@ -143,13 +145,13 @@ render json: @json_data
     def source_exist(dir, emp)
       from = dir + 'Emp'
       if !emp.nil?
-      from = from + emp
+        from = from + emp
       end
       from += '/'
       if !dir_exist(from)
         from = dir + 'EMP'
         if !emp.nil?
-        from = from + emp
+          from = from + emp
         end
         from += '/'
         if !dir_exist(from)
@@ -182,14 +184,14 @@ render json: @json_data
       # Street type
       if !source.ccodvia.blank?
         @street_type = StreetType.find_by_street_type_code(source.ccodvia)
-        if @street_type.nil
+        if @street_type.nil?
           @street_type = StreetType.first
         end
       end
       # ZIP code
       if !source.ccodpos.blank?
         @zipcode = Zipcode.find_by_zipcode(source.ccodpos)
-        if @zipcode.nil
+        if @zipcode.nil?
           @zipcode = Zipcode.first
         end
       end
@@ -199,22 +201,22 @@ render json: @json_data
       if !source.ccodconv.blank?
         pro_group = source.ccodconv
         @collective_agreement = CollectiveAgreement.find_by_nomina_id(source.ccodconv)
-        if @collective_agreement.nil
+        if @collective_agreement.nil?
           @collective_agreement = CollectiveAgreement.first
         end
       end
       # Professional group
       if !source.ccodcat.blank?
         pro_group = pro_group + '-' + source.ccodcat
-        @professional_group = ProfessionalGroup.find_by_nomina_id(source.ccodcat)
-        if @professional_group.nil
+        @professional_group = ProfessionalGroup.find_by_nomina_id(pro_group)
+        if @professional_group.nil?
           @professional_group = ProfessionalGroup.first
         end
       end
       # Contract type
       if !source.ccodcon.blank?
         @contract_type = ContractType.find_by_nomina_id(source.ccodcon)
-        if @contract_type.nil
+        if @contract_type.nil?
           @contract_type = ContractType.first
         end
       end
@@ -229,8 +231,16 @@ render json: @json_data
         #
         # Add new row data
         #
-        worker.first_name = source.cnomtra.gsub(/[^0-9A-Za-z ]/, '').titleize unless source.cnomtra.blank?
-        worker.last_name = source.capetra.gsub(/[^0-9A-Za-z ]/, '').titleize unless source.capetra.blank?
+        # Init uw vars to generate worker_code
+        first_name_uw = nil
+        last_name_uw = nil
+        first_name_uw = sanitize_string(source.cnomtra) unless source.cnomtra.blank?
+        last_name_uw = sanitize_string(source.capetra) unless source.capetra.blank?
+        #first_name_uw = source.cnomtra.gsub(/[^0-9A-Za-z ]/, '').titleize unless source.cnomtra.blank?
+        #last_name_uw = source.capetra.gsub(/[^0-9A-Za-z ]/, '').titleize unless source.capetra.blank?
+        # Assign data
+        worker.first_name = first_name_uw.titleize unless first_name_uw.blank?
+        worker.last_name = last_name_uw.titleize unless last_name_uw.blank?
         worker.fiscal_id = source.cdni unless source.cdni.blank?
         worker.borned_on = source.dfecnac unless source.dfecnac.blank?
         worker.street_type_id = @street_type.id unless @street_type.id.blank?
@@ -257,7 +267,9 @@ render json: @json_data
         worker.position = source.cpuesto.gsub(/[^0-9A-Za-z ]/, '').titleize unless source.cpuesto.blank?
         worker.gross_salary = source.nbruto unless source.nbruto.blank?
         # Mandatory worker code
-        worker.worker_code = generate_worker_code(source.capetra, source.cnomtra)
+        if !first_name_uw.nil? && !last_name_uw.nil?
+          worker.worker_code = generate_worker_code(first_name_uw, last_name_uw)
+        end
         # Mandatory e-mail and user info
         if !source.cemail.blank?
           worker.email = source.cemail
@@ -355,6 +367,15 @@ render json: @json_data
       set_defaults
     end
 
+    def sanitize_string(str)
+      if !str.blank?
+        str.tr('áéíóúñÁÉÍÓÚÑ', 'aeiounAEIOUN')
+        str.gsub(/[^0-9A-Za-z ]/, '')
+      else
+        str
+      end
+    end
+    
     def generate_worker_code(lastname, firstname)
       code = ''
       lastname1 = lastname.split(" ").first
