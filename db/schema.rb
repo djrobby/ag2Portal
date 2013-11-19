1bee8ed2e704fee09d2bb525b0aa05005b312cbc
spsisys <spsisys@gmail.com>
2013-11-19 21:54:11 +0100

    dbshema 20131119

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131111161752) do
+ActiveRecord::Schema.define(:version => 20131119181327) do
 
   create_table "activities", :force => true do |t|
     t.string   "description"
@@ -624,6 +624,7 @@ ActiveRecord::Schema.define(:version => 20131111161752) do
     t.datetime "updated_at",                                                 :null => false
     t.integer  "created_by"
     t.integer  "updated_by"
+    t.date     "expiration"
   end
 
   create_table "technicians", :force => true do |t|
