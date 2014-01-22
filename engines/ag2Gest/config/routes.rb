1972862e9766a4adfa06b450cf236b56e71f6f58
spsisys <spsisys@gmail.com>
2014-01-22 17:15:49 +0100

    Clients views done - Generate client code ???

--- engines/ag2Gest/config/routes.rb
+++ engines/ag2Gest/config/routes.rb
@@ -8,6 +8,18 @@ Ag2Gest::Engine.routes.draw do
     # Routes to search
 
     # Routes for jQuery POSTs
+    match 'clients/update_province_textfield_from_town/:id', :controller => 'clients', :action => 'update_province_textfield_from_town'
+    match 'clients/:id/update_province_textfield_from_town/:id', :controller => 'clients', :action => 'update_province_textfield_from_town'
+    match 'clients/update_province_textfield_from_zipcode/:id', :controller => 'clients', :action => 'update_province_textfield_from_zipcode'
+    match 'clients/:id/update_province_textfield_from_zipcode/:id', :controller => 'clients', :action => 'update_province_textfield_from_zipcode'
+    match 'clients/update_country_textfield_from_region/:id', :controller => 'clients', :action => 'update_country_textfield_from_region'
+    match 'clients/:id/update_country_textfield_from_region/:id', :controller => 'clients', :action => 'update_country_textfield_from_region'
+    match 'clients/update_region_textfield_from_province/:id', :controller => 'clients', :action => 'update_region_textfield_from_province'
+    match 'clients/:id/update_region_textfield_from_province/:id', :controller => 'clients', :action => 'update_region_textfield_from_province'
+    match 'clients/validate_fiscal_id_textfield/:id', :controller => 'clients', :action => 'validate_fiscal_id_textfield'
+    match 'clients/:id/validate_fiscal_id_textfield/:id', :controller => 'clients', :action => 'validate_fiscal_id_textfield'
+    match 'clients/update_code_textfield/:id', :controller => 'clients', :action => 'update_code_textfield'
+    match 'clients/:id/update_code_textfield/:id', :controller => 'clients', :action => 'update_code_textfield'
 
     # Resources
     resources :clients
