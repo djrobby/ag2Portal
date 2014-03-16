7ebe4051e3440433c67b3d09e05af2a67c81669c
spsisys <spsisys@gmail.com>
2014-03-16 12:23:49 +0100

    ag2Tech controllers II

--- engines/ag2Tech/app/controllers/ag2_tech/work_orders_controller.rb
+++ engines/ag2Tech/app/controllers/ag2_tech/work_orders_controller.rb
@@ -2,10 +2,15 @@ require_dependency "ag2_tech/application_controller"
 
 module Ag2Tech
   class WorkOrdersController < ApplicationController
+    before_filter :authenticate_user!
+    load_and_authorize_resource
+    # Helper methods for sorting
+    helper_method :sort_column
+
     # GET /work_orders
     # GET /work_orders.json
     def index
-      @work_orders = WorkOrder.all
+      @work_orders = WorkOrder.paginate(:page => params[:page], :per_page => per_page).order(sort_column + ' ' + sort_direction)
   
       respond_to do |format|
         format.html # index.html.erb
@@ -16,6 +21,7 @@ module Ag2Tech
     # GET /work_orders/1
     # GET /work_orders/1.json
     def show
+      @breadcrumb = 'read'
       @work_order = WorkOrder.find(params[:id])
   
       respond_to do |format|
@@ -27,6 +33,7 @@ module Ag2Tech
     # GET /work_orders/new
     # GET /work_orders/new.json
     def new
+      @breadcrumb = 'create'
       @work_order = WorkOrder.new
   
       respond_to do |format|
@@ -37,17 +44,20 @@ module Ag2Tech
   
     # GET /work_orders/1/edit
     def edit
+      @breadcrumb = 'update'
       @work_order = WorkOrder.find(params[:id])
     end
   
     # POST /work_orders
     # POST /work_orders.json
     def create
+      @breadcrumb = 'create'
       @work_order = WorkOrder.new(params[:work_order])
+      @work_order.created_by = current_user.id if !current_user.nil?
   
       respond_to do |format|
         if @work_order.save
-          format.html { redirect_to @work_order, notice: 'Work order was successfully created.' }
+          format.html { redirect_to @work_order, notice: crud_notice('created', @work_order) }
           format.json { render json: @work_order, status: :created, location: @work_order }
         else
           format.html { render action: "new" }
@@ -59,11 +69,14 @@ module Ag2Tech
     # PUT /work_orders/1
     # PUT /work_orders/1.json
     def update
+      @breadcrumb = 'update'
       @work_order = WorkOrder.find(params[:id])
+      @work_order.updated_by = current_user.id if !current_user.nil?
   
       respond_to do |format|
         if @work_order.update_attributes(params[:work_order])
-          format.html { redirect_to @work_order, notice: 'Work order was successfully updated.' }
+          format.html { redirect_to @work_order,
+                        notice: (crud_notice('updated', @work_order) + "#{undo_link(@work_order)}").html_safe }
           format.json { head :no_content }
         else
           format.html { render action: "edit" }
@@ -76,12 +89,23 @@ module Ag2Tech
     # DELETE /work_orders/1.json
     def destroy
       @work_order = WorkOrder.find(params[:id])
-      @work_order.destroy
-  
+
       respond_to do |format|
-        format.html { redirect_to work_orders_url }
-        format.json { head :no_content }
+        if @work_order_type.destroy
+          format.html { redirect_to work_orders_url,
+                      notice: (crud_notice('destroyed', @work_order) + "#{undo_link(@work_order)}").html_safe }
+          format.json { head :no_content }
+        else
+          format.html { redirect_to work_orders_url, alert: "#{@work_order.errors[:base].to_s}".gsub('["', '').gsub('"]', '') }
+          format.json { render json: @work_order.errors, status: :unprocessable_entity }
+        end
       end
+    end
+
+    private
+
+    def sort_column
+      WorkOrderLabor.column_names.include?(params[:sort]) ? params[:sort] : "order_no"
     end
   end
 end
