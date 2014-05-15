eac950c9acb7d8cc4ddcc8a9e5ac068bc7b90441
spsisys <spsisys@gmail.com>
2014-05-15 20:51:13 +0200

    Formatting numbers at ag2Logistics I

--- app/assets/javascripts/other-stuff.js
+++ app/assets/javascripts/other-stuff.js
@@ -14,3 +14,22 @@ function es_date(d) {
   var o = (day<10 ? '0' : '') + day + '/' + (month<10 ? '0' : '') + month + '/' + d.getFullYear();
   return o;
 }
+
+/*
+ * Format numeric string with specific decimal digits
+ * n parameter must be a number
+ * returns en-US formatted number
+ */
+function right_number(n, l) {
+  var r = n.replace(",", ".");
+  var p = r.indexOf('.');
+  var d = '';
+  if (p >= 0) {
+    d = r.substring(p + 1);
+    if (d.length > l) {
+      d = r.length - (d.length - l);
+      r = r.substring(0, d);
+    } 
+  }
+  return r;
+}
