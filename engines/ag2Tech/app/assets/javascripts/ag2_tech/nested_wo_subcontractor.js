ae07e414d00ac579d03e2e31b319418baed2c076
spsisys <spsisys@gmail.com>
2015-04-08 20:21:27 +0200

    Word order: Adding subcontractor to _form II

--- engines/ag2Tech/app/assets/javascripts/ag2_tech/nested_wo_subcontractor.js
+++ engines/ag2Tech/app/assets/javascripts/ag2_tech/nested_wo_subcontractor.js
@@ -53,8 +53,8 @@ var wo_s_formHandler = {
         wo_s_rowBuilder.addRow(wo_s_cfg.getTBodySelector(), inputFields);
 
         // Apply select2 to added row selects
-        $('select.wsel2').select2('destroy');
-        $('select.wsel2').select2({
+        $('select.ssel2').select2('destroy');
+        $('select.ssel2').select2({
           formatNoMatches: function(m) { return sel2NoMatches; },
           dropdownCssClass: 'shrinked',
           dropdownAutoWidth: true,
@@ -108,11 +108,11 @@ var wo_s_rowBuilder = function() {
             if (this.id.indexOf("s2") == -1) {
               // Apply CSS
               css = this.id;
-              if ($(this).hasClass('fsel2')) css = css + ' select wsel2';
+              if ($(this).hasClass('fsel2')) css = css + ' select ssel2';
               if ($(this).hasClass('number-text-field')) css = css + ' sub-number-text-field';
               if ($(this).hasClass('sub-disabled-field')) css = css + ' sub-disabled-field';
               if (css === this.id) css = css + ' sub-alfanumeric-text-field';
-              if (css.indexOf("wsel2") == -1) css = css + ' sub-bordered-input';
+              if (css.indexOf("ssel2") == -1) css = css + ' sub-bordered-input';
               css = css + ' string';
               $(this).removeAttr('class');
               $(this).addClass(css);
