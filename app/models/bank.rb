966a98c35f56a78dbcd29945ad875305414578d5
spsisys <spsisys@gmail.com>
2015-09-07 14:10:21 +0200

    Bank & Bank office models initialized (localization)

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,3 +1,31 @@
 class Bank < ActiveRecord::Base
   attr_accessible :code, :name, :swift
+
+  has_many :supplier_bank_accounts
+
+  has_paper_trail
+
+  validates :code,  :presence => true,
+                    :length => { :is => 4 },
+                    :format => { with: /\A\d+\Z/, message: :code_invalid },
+                    :uniqueness => true
+  validates :name,  :presence => true
+  validates :swift, :length => { :minimum => 8, :maximum => 11 }, :if => "!swift.blank?"
+
+  before_destroy :check_for_dependent_records
+
+  private
+
+  def check_for_dependent_records
+    # Check for supplier bank accounts
+    if supplier_bank_accounts.count > 0
+      errors.add(:base, I18n.t('activerecord.models.bank.check_for_supplier_bank_accounts'))
+      return false
+    end
+    # Check for client bank accounts
+    if client_bank_accounts.count > 0
+      errors.add(:base, I18n.t('activerecord.models.bank.check_for_client_bank_accounts'))
+      return false
+    end
+  end
 end
--- app/models/bank_office.rb
+++ app/models/bank_office.rb
@@ -6,5 +6,41 @@ class BankOffice < ActiveRecord::Base
   belongs_to :province
   belongs_to :region
   belongs_to :country
-  attr_accessible :building, :cellular, :code, :email, :extension, :fax, :floor, :floor_office, :name, :phone, :street_name, :street_number
+  attr_accessible :building, :cellular, :code, :email, :extension, :fax, :floor, :floor_office, :name, :phone,
+                  :street_name, :street_number, :bank_id, :street_type_id, :zipcode_id, :town_id, :province_id,
+                  :region_id, :country_id
+
+  has_many :supplier_bank_accounts
+
+  has_paper_trail
+
+  validates :code,          :presence => true,
+                            :length => { :is => 4 },
+                            :format => { with: /\A\d+\Z/, message: :code_invalid },
+                            :uniqueness => { :scope => :bank_id }
+  validates :name,          :presence => true
+  validates :swift,         :length => { :minimum => 8, :maximum => 11 }, :if => "!swift.blank?"
+  validates :street_type,   :presence => true
+  validates :zipcode,       :presence => true
+  validates :town,          :presence => true
+  validates :province,      :presence => true
+  validates :region,        :presence => true
+  validates :country,       :presence => true
+
+  before_destroy :check_for_dependent_records
+
+  private
+
+  def check_for_dependent_records
+    # Check for supplier bank accounts
+    if supplier_bank_accounts.count > 0
+      errors.add(:base, I18n.t('activerecord.models.bank_office.check_for_supplier_bank_accounts'))
+      return false
+    end
+    # Check for client bank accounts
+    if client_bank_accounts.count > 0
+      errors.add(:base, I18n.t('activerecord.models.bank_office.check_for_client_bank_accounts'))
+      return false
+    end
+  end
 end
