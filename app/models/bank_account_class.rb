e21f409a212537081ebd72ca1ca7e3a4fc1d2120
spsisys <spsisys@gmail.com>
2015-09-07 13:41:47 +0200

    Bank account class model initialized (localization)

--- app/models/bank_account_class.rb
+++ app/models/bank_account_class.rb
@@ -1,3 +1,26 @@
 class BankAccountClass < ActiveRecord::Base
   attr_accessible :name
+
+  has_many :supplier_bank_accounts
+
+  has_paper_trail
+
+  validates :name,  :presence => true
+
+  before_destroy :check_for_dependent_records
+
+  private
+
+  def check_for_dependent_records
+    # Check for supplier bank accounts
+    if supplier_bank_accounts.count > 0
+      errors.add(:base, I18n.t('activerecord.models.bank_account_class.check_for_supplier_bank_accounts'))
+      return false
+    end
+    # Check for client bank accounts
+    if client_bank_accounts.count > 0
+      errors.add(:base, I18n.t('activerecord.models.bank_account_class.check_for_client_bank_accounts'))
+      return false
+    end
+  end
 end
