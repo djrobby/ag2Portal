8b82538e616974fd6d89f756af4a1f01d0649c21
spsisys <spsisys@gmail.com>
2016-01-07 21:14:34 +0100

    ag2Admin home_controller

--- engines/ag2Admin/app/controllers/ag2_admin/home_controller.rb
+++ engines/ag2Admin/app/controllers/ag2_admin/home_controller.rb
@@ -4,11 +4,7 @@ module Ag2Admin
   class HomeController < ApplicationController
     def index
       @ag2teamnet_path, @ag2teamnet_target = website_path('ag2TeamNet', '_self')
-      session[:search] = nil
-      session[:letter] = nil      
-      session[:sort] = nil
-      session[:direction] = nil      
-      session[:ifilter] = nil      
+      reset_session_variables_for_filters
     end
   end
 end
