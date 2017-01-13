d28c10551ecfcf556273265aadd9d485d3a9899a
spsisys <spsisys@gmail.com>
2017-01-13 14:31:50 +0100

    SubscriberTariff scopes fixed

--- app/models/subscriber_tariff.rb
+++ app/models/subscriber_tariff.rb
@@ -8,12 +8,14 @@ class SubscriberTariff < ActiveRecord::Base
   validates :starting_at, :presence => true
 
   # Scopes
-  scope :belongs_to_subscriber, -> s { where(subscriber_id: s) }
-  scope :belongs_to_tariff, -> t { where(tariff_id: t) }
-  scope :availables, -> { where('ending_at IS NULL OR ending_at > ?', Date.today) }
-  scope :availables_to_subscriber, -> s { where("subscriber_id = ? AND (tariffs.ending_at IS NULL OR tariffs.ending_at > ?)", s, Date.today)}
-  scope :availables_to_tariff, -> t { where("tariff_id = ? AND (tariffs.ending_at IS NULL OR tariffs.ending_at > ?)", t, Date.today)}
-  scope :availables_to_subscriber_tariff, -> s, t { where("subscriber_id = ? AND tariff_id = ? AND (tariffs.ending_at IS NULL OR tariffs.ending_at > ?)", s, t, Date.today)}
+  scope :by_code, -> { order(:client_code) }
+  #
+  scope :belongs_to_subscriber, -> s { joins(:tariff).where(subscriber_id: s).order('subscriber_tariffs.starting_at DESC, tariffs.billable_item_id') }
+  scope :belongs_to_tariff, -> t { joins(:tariff).where(tariff_id: t).order('subscriber_tariffs.starting_at DESC, tariffs.billable_item_id') }
+  scope :availables, -> { joins(:tariff).where('subscriber_tariffs.ending_at IS NULL OR subscriber_tariffs.ending_at > ?', Date.today).order('subscriber_tariffs.starting_at DESC, tariffs.billable_item_id') }
+  scope :availables_to_subscriber, -> s { joins(:tariff).where("subscriber_id = ? AND (subscriber_tariffs.ending_at IS NULL OR subscriber_tariffs.ending_at > ?)", s, Date.today).order('subscriber_tariffs.starting_at DESC, tariffs.billable_item_id') }
+  scope :availables_to_tariff, -> t { joins(:tariff).where("tariff_id = ? AND (subscriber_tariffs.ending_at IS NULL OR subscriber_tariffs.ending_at > ?)", t, Date.today).order('subscriber_tariffs.starting_at DESC, tariffs.billable_item_id') }
+  scope :availables_to_subscriber_tariffs, -> s, t { joins(:tariff).where("subscriber_id = ? AND tariff_id = ? AND (subscriber_tariffs.ending_at IS NULL OR subscriber_tariffs.ending_at > ?)", s, t, Date.today).order('subscriber_tariffs.starting_at DESC, tariffs.billable_item_id') }
 
   before_validation :assign_at
 
@@ -24,7 +26,7 @@ class SubscriberTariff < ActiveRecord::Base
   private
 
   def assign_at
-    self.starting_at = tariff.starting_at
-    self.ending_at = tariff.ending_at
+    self.starting_at = tariffs.starting_at
+    self.ending_at = tariffs.ending_at
   end
 end
