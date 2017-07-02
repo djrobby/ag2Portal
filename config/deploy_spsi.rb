77393a1df8f30b059e9b187e9d128c4a331d0013
spsisys <spsisys@gmail.com>
2017-07-02 12:15:22 +0200

    LAMP deploy 20170702_1215

--- config/deploy_spsi.rb
+++ config/deploy_spsi.rb
@@ -47,4 +47,14 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
