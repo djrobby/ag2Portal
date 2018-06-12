dd22dcfa34edd668000e7fd9fda2f4fe1c549f5d
spsisys <spsisys@gmail.com>
2018-06-12 13:00:46 +0200

    nested_cl_accounts.js fixed for IBAN validation

--- engines/ag2Gest/app/assets/javascripts/ag2_gest/nested_cl_account.js
+++ engines/ag2Gest/app/assets/javascripts/ag2_gest/nested_cl_account.js
@@ -10,156 +10,174 @@
  */
 
 var cl_accountFieldsUI = {
-    init: function(sel2NoMatches, invalidIBAN, tryAgain) {
-        var validationSettings = {
-            errorMessagePosition : 'element'
-        };
+  init: function (sel2NoMatches, invalidIBAN, tryAgain) {
+    var validationSettings = {
+      errorMessagePosition: 'element'
+    };
 
-        $('#addAccountButton').on('click', function(e) {
-          e.stopPropagation();
-            var iban = $("#fnt-iban").val();
-            // var isValidIBAN = validate_iban_new(invalidIBAN, tryAgain, $('#fnt-iban').val());
-            var isValid = $('#new-account-fields').validate(false, validationSettings);
-            if (!isValid || !iban.length) {
-                return false;
-            }
-            if (iban.length){
-              e.preventDefault();
-              var isValid = true;
-              jQuery.getJSON('https://openiban.com/validate/' + iban, function (data) {
-                isValid = data.valid
-                if (data.valid == false) {
-                  alert(data.iban + ' ' + invalidIBAN + '\n' + tryAgain);
-                  $("#fnt-iban").val("");
-                  return false;
-                } else {
-                  alert(data.iban + ' OK');
-                  cl_account_formHandler.appendFields(sel2NoMatches);
-                  cl_account_formHandler.hideForm(); 
-                }
-              });
-            }
+    $('#addAccountButton').on('click', function (e) {
+      e.stopPropagation();
+      var iban = $("#fnt-iban").val();
+      // var isValidIBAN = validate_iban_new(invalidIBAN, tryAgain, $('#fnt-iban').val());
+      var isValid = $('#new-account-fields').validate(false, validationSettings);
+      if (!isValid || !iban.length) {
+        return false;
+      }
+      if (iban.length) {
+        e.preventDefault();
+        var isValid = true;
+        jQuery.getJSON('cl_validate_iban/' + iban, function (data) {
+          isValid = data.valid
+          if (data.valid == false) {
+            alert(data.iban + ' ' + data.invalidIBAN + '\n' + data.tryAgain);
+            $("#fnt-iban").val("");
+            return false;
+          } else {
+            alert(data.iban + ' OK');
+            cl_account_formHandler.appendFields(sel2NoMatches);
+            cl_account_formHandler.hideForm();
+          }
         });
+        // jQuery.getJSON('https://openiban.com/validate/' + iban, function (data) {
+        //   isValid = data.valid
+        //   if (data.valid == false) {
+        //     alert(data.iban + ' ' + invalidIBAN + '\n' + tryAgain);
+        //     $("#fnt-iban").val("");
+        //     return false;
+        //   } else {
+        //     alert(data.iban + ' OK');
+        //     cl_account_formHandler.appendFields(sel2NoMatches);
+        //     cl_account_formHandler.hideForm();
+        //   }
+        // });
+      }
+    });
 
-        $('#cancelAccountButton').on('click', function(e) {
-          cl_account_formHandler.removeFields();
-          cl_account_formHandler.hideForm();
-        });
-    }
+    $('#cancelAccountButton').on('click', function (e) {
+      cl_account_formHandler.removeFields();
+      cl_account_formHandler.hideForm();
+    });
+  }
 };
 
 var cl_account_cfg = {
-    formId: '#new-account-fields',
-    tableId: '#accounts-table',
-    inputFieldClassSelector: '.field',
-    getTBodySelector: function() {
-        return this.tableId + ' tbody';
-    }
+  formId: '#new-account-fields',
+  tableId: '#accounts-table',
+  inputFieldClassSelector: '.field',
+  getTBodySelector: function () {
+    return this.tableId + ' tbody';
+  }
 };
 
 var cl_account_formHandler = {
-    // Public method for adding a new row to the table.
-    appendFields: function (sel2NoMatches) {
-        // Get a handle on all the input fields in the form and detach them from
-        // the DOM (we'll attach them later).
-        var inputFields = $(cl_account_cfg.formId + ' ' + cl_account_cfg.inputFieldClassSelector);
-        inputFields.detach();
+  // Public method for adding a new row to the table.
+  appendFields: function (sel2NoMatches) {
+    // Get a handle on all the input fields in the form and detach them from
+    // the DOM (we'll attach them later).
+    var inputFields = $(cl_account_cfg.formId + ' ' + cl_account_cfg.inputFieldClassSelector);
+    inputFields.detach();
 
-        // Build the row and add it to the end of the table.
-        cl_account_rowBuilder.addRow(cl_account_cfg.getTBodySelector(), inputFields);
+    // Build the row and add it to the end of the table.
+    cl_account_rowBuilder.addRow(cl_account_cfg.getTBodySelector(), inputFields);
 
-        // Apply select2 to added row selects
-        $('select.wsel2').select2('destroy');
-        $('select.wsel2').select2({
-          formatNoMatches: function(m) { return sel2NoMatches; },
-          dropdownCssClass: 'shrinked',
-          dropdownAutoWidth: true,
-          containerCssClass: 'sub-select2-field'
-        });
-    },
+    // Apply select2 to added row selects
+    $('select.wsel2').select2('destroy');
+    $('select.wsel2').select2({
+      formatNoMatches: function (m) {
+        return sel2NoMatches;
+      },
+      dropdownCssClass: 'shrinked',
+      dropdownAutoWidth: true,
+      containerCssClass: 'sub-select2-field'
+    });
+  },
 
-    // Public method for remove a new row when cancel button has been clicked.
-    removeFields: function () {
-        // Get a handle on all the input fields in the form and detach them from
-        // the DOM (we'll attach them later).
-        var inputFields = $(cl_account_cfg.formId + ' ' + cl_account_cfg.inputFieldClassSelector);
-        inputFields.detach();
+  // Public method for remove a new row when cancel button has been clicked.
+  removeFields: function () {
+    // Get a handle on all the input fields in the form and detach them from
+    // the DOM (we'll attach them later).
+    var inputFields = $(cl_account_cfg.formId + ' ' + cl_account_cfg.inputFieldClassSelector);
+    inputFields.detach();
 
-        // Change value of _destroy field
-        $(inputFields).map(function() {
-          if (this.id.indexOf("_destroy") != -1) {
-            $(this).val("1");
-          }
-        });
-    },
+    // Change value of _destroy field
+    $(inputFields).map(function () {
+      if (this.id.indexOf("_destroy") != -1) {
+        $(this).val("1");
+      }
+    });
+  },
 
-    // Public method for hiding the data entry fields.
-    hideForm: function() {
-        // Update and display totals NOT HERE!
-        //$('#accounts-table').trigger('totals');
-        // Hide modal
-        $(cl_account_cfg.formId).modal('hide');
-    }
+  // Public method for hiding the data entry fields.
+  hideForm: function () {
+    // Update and display totals NOT HERE!
+    //$('#accounts-table').trigger('totals');
+    // Hide modal
+    $(cl_account_cfg.formId).modal('hide');
+  }
 };
 
-var cl_account_rowBuilder = function() {
-    // Private property that define the default <TR> element text.
-    var row = $('<tr>', { class: 'fields' });
+var cl_account_rowBuilder = function () {
+  // Private property that define the default <TR> element text.
+  var row = $('<tr>', {
+    class: 'fields'
+  });
 
-    // Public property that describes the "Remove" link.
-    var link = $('<a>', {
-        href: '#',
-        onclick: 'remove_fields(this); return false;'
-    }).append($('<i>', { class: 'icon-trash' }));
+  // Public property that describes the "Remove" link.
+  var link = $('<a>', {
+    href: '#',
+    onclick: 'remove_fields(this); return false;'
+  }).append($('<i>', {
+    class: 'icon-trash'
+  }));
 
-    // A private method for building a <TR> w/the required data.
-    var buildRow = function(fields) {
-        var newRow = row.clone();
-        var newLink = link.clone();
+  // A private method for building a <TR> w/the required data.
+  var buildRow = function (fields) {
+    var newRow = row.clone();
+    var newLink = link.clone();
 
-        // fields
-        $(fields).map(function() {
-            var id = '';
-            var css = '';
-            // Add only if not select2 link
-            if (this.id.indexOf("s2") == -1) {
-              // Apply CSS
-              id = this.id;
-              if ($(this).hasClass('fsel2')) css = css + ' select wsel2';
-              if ($(this).hasClass('number-text-field')) css = css + ' sub-number-text-field';
-              if ($(this).hasClass('sub-disabled-field')) css = css + ' sub-disabled-field';
-              if (css === '') css = css + ' sub-alfanumeric-text-field';
-              if (css.indexOf("wsel2") == -1) css = css + ' sub-bordered-input';
-              css = css + ' string ' + id;
-              $(this).removeAttr('class');
-              $(this).removeAttr('id');
-              $(this).addClass(css);
-              // Add new column to row
-              if (id == "fnt-iban") {
-                $(this).attr('readonly', 'true')
-              }
-              var td = $('<td/>').append($(this));
-              // If destroy field, add delete link also
-              if (id.indexOf("_destroy") != -1) {
-                var td = $('<td/>').append($(this), newLink);
-              }
-              td.appendTo(newRow);
-            }
-        });
+    // fields
+    $(fields).map(function () {
+      var id = '';
+      var css = '';
+      // Add only if not select2 link
+      if (this.id.indexOf("s2") == -1) {
+        // Apply CSS
+        id = this.id;
+        if ($(this).hasClass('fsel2')) css = css + ' select wsel2';
+        if ($(this).hasClass('number-text-field')) css = css + ' sub-number-text-field';
+        if ($(this).hasClass('sub-disabled-field')) css = css + ' sub-disabled-field';
+        if (css === '') css = css + ' sub-alfanumeric-text-field';
+        if (css.indexOf("wsel2") == -1) css = css + ' sub-bordered-input';
+        css = css + ' string ' + id;
+        $(this).removeAttr('class');
+        $(this).removeAttr('id');
+        $(this).addClass(css);
+        // Add new column to row
+        if (id == "fnt-iban") {
+          $(this).attr('readonly', 'true')
+        }
+        var td = $('<td/>').append($(this));
+        // If destroy field, add delete link also
+        if (id.indexOf("_destroy") != -1) {
+          var td = $('<td/>').append($(this), newLink);
+        }
+        td.appendTo(newRow);
+      }
+    });
 
-        return newRow;
-    };
+    return newRow;
+  };
 
-    // A public method for building a row and attaching it to the end of a
+  // A public method for building a row and attaching it to the end of a
   // <TBODY> element.
-    var attachRow = function(tableBody, fields) {
-        var row = buildRow(fields);
-        $(row).appendTo($(tableBody));
-    };
+  var attachRow = function (tableBody, fields) {
+    var row = buildRow(fields);
+    $(row).appendTo($(tableBody));
+  };
 
-    // Only expose public methods/properties.
-    return {
-        addRow: attachRow,
-        link: link
-    };
+  // Only expose public methods/properties.
+  return {
+    addRow: attachRow,
+    link: link
+  };
 }();
